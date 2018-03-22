ead86b98d965fda42783bca04127ca377023bb8d
Sean West <swest06@dcs.bbk.ac.uk>
2018-03-22 23:06:41 +0000

    Finish Exercise 9

--- Exercise09.py
+++ Exercise09.py
@@ -4,5 +4,20 @@ while num > 0:
     num -= 1
     file = input().split()
     lst = file[1:]
+    for i, e in enumerate(lst):
+        if e == "R":
+            lst[i] = "read"
+        elif e == "W":
+            lst[i] = "write"
+        else:
+            lst[i] = "execute"      
     d[file[0]] = lst
-    print(d)
+
+num2 = int(input())
+while num2 > 0:
+    num2 -= 1
+    file2 = input().split()
+    if file2[0] in d[file2[1]]:
+        print("OK")
+    else:
+        print("Access denied")
